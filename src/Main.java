066ead6e2912fc1eadef34dfa07d354cd77ee117
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-10 12:50:16 +0900

    Describe case 0:Circle in Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,29 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.print("図形描画[ 0:円 2:線 3:三角形 4:長方形 44:正方形 ] : >");
 		int input = new Scanner(System.in).nextInt();
-		switch(input) {
-		case 0:
+		switch (input) {
+			case 0:
+				Circle c1 = new Circle(100, 100, 20);
+				c1.draw();
+				System.out.println("周囲の長さは、" + c1.perimeter());
+				break;
+			case 2:
+				Line l1 = new Line(0, 0, 100, 100);
+				l1.draw();
+				System.out.println("周囲の長さは、" + l1.perimeter());
+				break;
+			case 3:
 
-			break;
-		case 2:
-			Line l1 = new Line(0, 0, 100, 100);
-			l1.draw();
-			System.out.println("周囲の長さは、"+l1.perimeter());
-			break;
-		case 3:
+				break;
+			case 4:
 
-			break;
-		case 4:
+				break;
+			case 44:
 
-			break;
-		case 44:
-
-			break;
-		default:
-			System.out.println("その値は無効です");
-			break;
+				break;
+			default:
+				System.out.println("その値は無効です");
+				break;
 		}
-
 	}
 }
